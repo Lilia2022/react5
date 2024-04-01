16af2fb46e391356b68d67690cb891c092855033
Lilia2022 <razafimiadana21411@iiitd.ac.in>
2024-04-01 20:20:52 +0530

    last

--- react/src/UserGreetings.jsx
+++ react/src/UserGreetings.jsx
@@ -1,6 +1,17 @@
 
 function UserGreetings(props){
-    return(props.isLoggedIn ? <h2 className="welcome-message">Welcome {props.username}</h2> : <h2 className="login-prompt">Please login if you want to continue</h2>)
+    const welcomeMessage = <h2 className="welcome-message">
+                            Welcome {props.username}
+                            </h2> 
+    const loginPrompt = <h2 className="login-prompt">
+                        Please login if you want to continue
+                        </h2>
+
+    return (props.isLoggIn ? welcomeMessage : loginPrompt)
+    
+    //return(props.isLoggedIn ? <h2 className="welcome-message">Welcome {props.username}</h2> : <h2 className="login-prompt">Please login if you want to continue</h2>)
+
+
     // if(props.isLoggedIn){
     //     return <h2>Welcome {props.username}</h2>
     // }
