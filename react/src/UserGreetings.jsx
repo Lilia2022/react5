3b5b326fee8ff6a720ee9d567fc658f1d22bc7ee
Lilia2022 <razafimiadana21411@iiitd.ac.in>
2024-04-01 20:16:08 +0530

    commit

--- react/src/UserGreetings.jsx
+++ react/src/UserGreetings.jsx
@@ -1,6 +1,6 @@
 
 function UserGreetings(props){
-    return(props.isLoggedIn ? <h2>Welcome {props.username}</h2> : <h2>Please login if you want to continue</h2>)
+    return(props.isLoggedIn ? <h2 className="welcome-message">Welcome {props.username}</h2> : <h2 className="login-prompt">Please login if you want to continue</h2>)
     // if(props.isLoggedIn){
     //     return <h2>Welcome {props.username}</h2>
     // }
